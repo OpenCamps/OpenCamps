011c888eebdf7339558f6b9e9354501517fff004
Willy Karam <admin@willykaram.com>
2017-03-06 07:10:51 -0500

    2017 revs

---
+++ 2017/js/listConferences.js
@@ -0,0 +1,531 @@
+﻿getConferences([
+  //01  
+  {
+    "name": "Agency Camp",
+    "description": "",
+    "camp": "agency",
+    "dates": "",
+    "icon": "icon-agency.svg",
+    "website": "http://agency.camp",
+    "twitter": "http://twitter.com/agency_camp",
+    "facebook": "",
+    "github": "http://github.com/AgencyCamp",
+    "email": "info@agency.camp"
+  },
+
+  //02  
+  {
+    "name": "AI Camp",
+    "description": "",
+    "camp": "ai",
+    "dates": "",
+    "icon": "icon-ai.svg",
+    "website": "http://ai.camp",
+    "twitter": "http://twitter.com/ai_camp_nyc",
+    "facebook": "",
+    "github": "http://github.com/AICamp",
+    "email": "info@agency.camp"
+  },
+  
+  //03  
+  {
+    "name": "Android Camp",
+    "description": "",
+    "camp": "android",
+    "dates": "",
+    "icon": "icon-android.svg",
+    "website": "http://androidcamp.io",
+    "twitter": "http://twitter.com/AndroidCampNYC",
+    "facebook": "",
+    "github": "http://github.com/AndroidCamp",
+    "email": "info@androidcamp.io"
+  },
+  
+  //04
+  {
+    "name": "Angular Camp",
+    "description": "",
+    "camp": "angular",
+    "dates": "",
+    "icon": "icon-angular.svg",
+    "website": "http://Angular.Camp",
+    "twitter": "http://twitter.com/Angular_Camp",
+    "facebook": "https://www.facebook.com/Angular-Camp-255028974857374",
+    "github": "http://github.com/AngularCamp",
+    "email": "info@angular.camp"
+  },
+
+  //05
+  {
+    "name": "Apps Camp",
+    "description": "",
+    "camp": "apps",
+    "dates": "",
+    "icon": "icon-apps.svg",
+    "website": "http://AppsCamp.io",
+    "twitter": "http://twitter.com/AppsCamp",
+    "facebook": "https://www.facebook.com/Apps-Camp-774489922605046",
+    "github": "http://github.com/AppsCamp",
+    "email": "info@appscamp.io"
+  },
+
+  //06
+  {
+    "name": "Capital Camp",
+    "description": "",
+    "camp": "capital",
+    "dates": "",
+    "icon": "icon-capital.png",
+    "website": "http://AppsCamp.io",
+    "twitter": "http://twitter.com/AppsCamp",
+    "facebook": "https://www.facebook.com/Apps-Camp-774489922605046",
+    "github": "http://github.com/AppsCamp",
+    "email": "info@appscamp.io"
+  },
+
+  //07
+  {
+    "name": "Community Camp",
+    "description": "",
+    "camp": "community",
+    "dates": "",
+    "icon": "icon-community.png",
+    "website": "http://Community.Camp",
+    "twitter": "http://twitter.com/OSCommunityCamp",
+    "facebook": "https://www.facebook.com/Community-Camp-589751477856795",
+    "github": "http://github.com/CommunityCamp",
+    "email": "info@community.camp"
+  },
+
+  //08
+  {
+    "name": "Commerce Camp",
+    "description": "",
+    "camp": "commerce",
+    "dates": "",
+    "icon": "icon-commerce.png",
+    "website": "http://Community.Camp",
+    "twitter": "http://twitter.com/OSCommunityCamp",
+    "facebook": "https://www.facebook.com/Community-Camp-589751477856795",
+    "github": "http://github.com/CommunityCamp",
+    "email": "info@community.camp"
+  },
+
+  //09
+  {
+    "name": "Data Viz Camp",
+    "description": "",
+    "camp": "dataviz",
+    "dates": "",
+    "icon": "icon-dataviz.png",
+    "website": "http://DataViz.Camp",
+    "twitter": "http://twitter.com/DataVizCamp",
+    "facebook": "http://facebook.com/datavizcamp",
+    "github": "http://github.com/DataVizCamp",
+    "email": "info@dataviz.camp"
+  },
+
+  {
+    "name": "Database Camp",
+    "description": "",
+    "camp": "database",
+    "dates": "",
+    "icon": "icon-database.png",
+    "website": "http://database.camp",
+    "twitter": "http://twitter.com/Database_Camp",
+    "facebook": "https://www.facebook.com/Database-Camp-1695352507388925",
+    "github": "http://github.com/DatabaseCamp",
+    "email": "info@database.camp"
+  },
+
+  {
+    "name": "Design Camp",
+    "description": "",
+    "camp": "design",
+    "dates": "",
+    "icon": "icon-design.svg",
+    "website": "http://DevOpsCamp.nyc",
+    "twitter": "https://twitter.com/Dev_Ops_Camp",
+    "facebook": "https://www.facebook.com/Dev-Ops-Camp-1708307602775959",
+    "github": "http://github.com/DevOpsCamp",
+    "email": "info@devopscamp.nyc"
+  },
+
+  {
+    "name": "Dev Ops Camp",
+    "description": "A community-run single day, single track conference focused on open source (mostly backend) dev ops",
+    "camp": "devops",
+    "dates": "Sat Jul 9",
+    "icon": "icon-devops.svg",
+    "website": "http://DevOpsCamp.nyc",
+    "twitter": "https://twitter.com/Dev_Ops_Camp",
+    "facebook": "https://www.facebook.com/Dev-Ops-Camp-1708307602775959",
+    "github": "http://github.com/DevOpsCamp",
+    "email": "info@devopscamp.nyc"
+  },
+
+  {
+    "name": "Diversity Camp",
+    "description": "",
+    "camp": "diversity",
+    "dates": "",
+    "icon": "icon-diversity.png",
+    "website": "http://DevOpsCamp.nyc",
+    "twitter": "https://twitter.com/Dev_Ops_Camp",
+    "facebook": "https://www.facebook.com/Dev-Ops-Camp-1708307602775959",
+    "github": "http://github.com/DevOpsCamp",
+    "email": "info@devopscamp.nyc"
+  },
+
+  {
+    "name": "Drupal NYC Camp",
+    "description": "",
+    "camp": "drupal",
+    "dates": "",
+    "icon": "icon-drupal.svg",
+    "website": "http://nyccamp.org",
+    "twitter": "http://twitter.com/NYCCamp_org",
+    "facebook": "http://facebook.com/NYCCamp",
+    "github": "http://github.com/NYC-Camp",
+    "email": "info@drupalnyc.camp"
+  },
+
+  {
+    "name": "EdTech Camp",
+    "description": "",
+    "camp": "edtech",
+    "dates": "",
+    "icon": "icon-edtech.png",
+    "website": "http://nyccamp.org",
+    "twitter": "http://twitter.com/NYCCamp_org",
+    "facebook": "http://facebook.com/NYCCamp",
+    "github": "http://github.com/NYC-Camp",
+    "email": "info@drupalnyc.camp"
+  },
+
+  {
+    "name": "Elixir Camp",
+    "description": "",
+    "camp": "elixir",
+    "dates": "",
+    "icon": "icon-elixir.png",
+    "website": "http://ElixirCamp.io",
+    "twitter": "http://twitter.com/ElixirCamp",
+    "facebook": "https://www.facebook.com/Elixir-Camp-1051659851560763/",
+    "github": "http://github.com/ElixirCamp",
+    "email": "info@elixircamp.io"
+  },
+
+//  {
+//    "name": "Ember Camp",
+//    "description": "",
+//    "camp": "ember",
+//    "dates": "",
+//    "icon": "icon-ember.svg",
+//    "website": "http://nyccamp.org",
+//    "twitter": "http://twitter.com/NYCCamp_org",
+//    "facebook": "http://facebook.com/NYCCamp",
+//    "github": "http://github.com/NYC-Camp",
+//    "email": "info@drupalnyc.camp"
+//  },
+
+  {
+    "name": "Erlang Camp",
+    "description": "",
+    "camp": "erlang",
+    "dates": "",
+    "icon": "icon-erlang.svg",
+    "website": "http://Erlang.camp",
+    "twitter": "http://twitter.com/ErlangCamp",
+    "facebook": "",
+    "github": "http://github.com/ErlangCamp",
+    "email": "info@erlang.camp"
+  },
+
+  {
+    "name": "Front End Camp",
+    "description": "",
+    "camp": "frontend",
+    "dates": "",
+    "icon": "icon-frontend.png",
+    "website": "http://FrontEndCamp.io",
+    "twitter": "http://twitter.com/FrontEndCamp",
+    "facebook": "https://www.facebook.com/Front-End-Camp-1010842225663786/",
+    "github": "http://github.com/FrontEndCamp",
+    "email": "info@frontendcamp.io"
+  },
+
+  {
+    "name": "Go Camp",
+    "description": "",
+    "camp": "go",
+    "dates": "",
+    "icon": "icon-go.svg",
+    "website": "http://GoCamp.io",
+    "twitter": "https://twitter.com/GoCampNYC/",
+    "facebook": "https://www.facebook.com/Go-Camp-1644250012510407/",
+    "github": "http://github.com/GoCamp",
+    "email": "info@gocamp.io"
+  },
+
+  {
+    "name": "Gov Camp",
+    "description": "",
+    "camp": "gov",
+    "dates": "",
+    "icon": "icon-gov.svg",
+    "website": "http://Civic.Camp",
+    "twitter": "http://twitter.com/Civic_Camp",
+    "facebook": "https://www.facebook.com/Civic-Camp-1062624427127104/",
+    "github": "http://github.com/Civic_Camp",
+    "email": "info@civic.camp"
+  },
+
+  {
+    "name": "Hack for Humanity",
+    "description": "",
+    "camp": "h4h",
+    "dates": "",
+    "icon": "icon-h4h.png",
+    "website": "",
+    "twitter": "",
+    "facebook": "",
+    "github": "",
+    "email": ""
+  },
+
+  {
+    "name": "Libraries Camp",
+    "description": "",
+    "camp": "libraries",
+    "dates": "",
+    "icon": "icon-libraries.png",
+    "website": "http://MakerCamp.nyc",
+    "twitter": "http://twitter.com/maker_camp",
+    "facebook": "https://www.facebook.com/Maker-Camp-1092305667499363",
+    "github": "http://github/Maker-Camp",
+    "email": "info@makercamp.io"
+  },
+
+  {
+    "name": "Maker Camp",
+    "description": "",
+    "camp": "maker",
+    "dates": "",
+    "icon": "icon-maker.svg",
+    "website": "http://MakerCamp.nyc",
+    "twitter": "http://twitter.com/maker_camp",
+    "facebook": "https://www.facebook.com/Maker-Camp-1092305667499363",
+    "github": "http://github/Maker-Camp",
+    "email": "info@makercamp.io"
+  },
+
+  {
+    "name": "Maps Camp",
+    "description": "",
+    "camp": "maps",
+    "dates": "",
+    "icon": "icon-maps.png",
+    "website": "http://MapsCamp.io",
+    "twitter": "https://twitter.com/maps_camp",
+    "facebook": "https://www.facebook.com/Maps-Camp-1709330742686181/",
+    "github": "http://github.com/MapsCamp",
+    "email": "info@mapscamp.io"
+  },
+
+  {
+    "name": "Meteor Camp NYC",
+    "description": "",
+    "camp": "meteor",
+    "dates": "",
+    "icon": "icon-meteor.svg",
+    "website": "http://meteorcamp.nyc",
+    "twitter": "http://twitter.com/meteorcampnyc",
+    "facebook": "https://www.facebook.com/Meteor-Camp-NYC-601343803355126",
+    "github": "https://github.com/MeteorCampNYC",
+    "email": "info@meteorcamp.nyc"
+  },
+
+  {
+    "name": "Nerves Camp",
+    "description": "",
+    "camp": "nerves",
+    "dates": "",
+    "icon": "icon-nerves.svg",
+    "website": "http://nerves.camp",
+    "twitter": "http://twitter.com/nervescamp",
+    "facebook": "",
+    "github": "http://github.com/NervesCamp",
+    "email": "info@nerves.camp"
+  },
+
+  {
+    "name": "Next Gen Camp",
+    "description": "",
+    "camp": "nextgen",
+    "dates": "",
+    "icon": "icon-nextgen.png",
+    "website": "http://nextgen.camp",
+    "twitter": "http://twitter.com/NextGen_Camp",
+    "facebook": "https://www.facebook.com/NextGen-Camp-483986091806286",
+    "github": "http://github.com/NextGenCamp",
+    "email": "info@nextgen.camp"
+  },
+
+  {
+    "name": "Node Camp",
+    "description": "",
+    "camp": "node",
+    "dates": "",
+    "icon": "icon-node.svg",
+    "website": "http://nodecamp.io",
+    "twitter": "http://twitter.com/node_camp",
+    "facebook": "https://www.facebook.com/Node-Camp-1147042711997175/",
+    "github": "http://github.com/nodecamp",
+    "email": "info@nodecamp.io"
+  },
+
+  {
+    "name": "NPO-NGO Camp",
+    "description": "",
+    "camp": "npongo",
+    "dates": "",
+    "icon": "icon-npongo.png",
+    "website": "http://nyc.phoenix.camp",
+    "twitter": "http://twitter.com/phoenix_camp",
+    "facebook": "http://facebook.com/nyc.phoenix.camp",
+    "github": "http://github.com/phoenixcamp",
+    "email": "info@nyc.phoenix.camp"
+  },
+
+  {
+    "name": "Phoenix Camp",
+    "description": "",
+    "camp": "phoenix",
+    "dates": "",
+    "icon": "icon-phoenix.png",
+    "website": "http://nyc.phoenix.camp",
+    "twitter": "http://twitter.com/phoenix_camp",
+    "facebook": "http://facebook.com/nyc.phoenix.camp",
+    "github": "http://github.com/phoenixcamp",
+    "email": "info@nyc.phoenix.camp"
+  },
+  
+  {
+    "name": "Rails unCamp",
+    "description": "",
+    "camp": "rails",
+    "dates": "",
+    "icon": "icon-rails.png",
+    "website": "http://railscamp.io",
+    "twitter": "https://twitter.com/RailsCampNYC",
+    "facebook": "https://www.facebook.com/Rails-Camp-1587900534855137",
+    "github": "http://github.com/railscamp",
+    "email": "info@railscamp.io"
+  },
+
+  {
+    "name": "React Camp",
+    "description": "",
+    "camp": "react",
+    "dates": "",
+    "icon": "icon-react.svg",
+    "website": "http://reactcamp.org",
+    "twitter": "http://twitter.com/react_camp",
+    "facebook": "https://www.facebook.com/React-Camp-952898308139084/",
+    "github": "http://github.com/reactcamp",
+    "email": "info@reactcamp.io"
+  },
+
+ {
+    "name": "Search Camp",
+    "description": "",
+    "camp": "search",
+    "dates": "",
+    "icon": "icon-search.svg",
+    "website": "http://searchcamp.io",
+    "twitter": "https://twitter.com/searchcampio",
+    "facebook": "https://www.facebook.com/Search-Camp-462121383977544",
+    "github": "http://github.com/searchcamp",
+    "email": "info@searchcamp.io"
+  },
+
+  {
+    "name": "Security Camp",
+    "description": "",
+    "camp": "security",
+    "dates": "",
+    "icon": "icon-security.png",
+    "website": "http://securitycamp.io",
+    "twitter": "https://twitter.com/searchcampio",
+    "facebook": "https://www.facebook.com/Search-Camp-462121383977544",
+    "github": "http://github.com/searchcamp",
+    "email": "info@searchcamp.io"
+  },
+
+  {
+    "name": "Startup Camp",
+    "description": "",
+    "camp": "startup",
+    "dates": "",
+    "icon": "icon-startup.png",
+    "website": "http://securitycamp.io",
+    "twitter": "https://twitter.com/searchcampio",
+    "facebook": "https://www.facebook.com/Search-Camp-462121383977544",
+    "github": "http://github.com/searchcamp",
+    "email": "info@searchcamp.io"
+  },
+
+  {
+    "name": "Swift Camp",
+    "description": "",
+    "camp": "swift",
+    "dates": "",
+    "icon": "icon-swift.svg",
+    "website": "http://swiftcamp.io",
+    "twitter": "https://twitter.com/swiftcamo",
+    "facebook": "",
+    "github": "http://github.com/swiftcamp",
+    "email": "info@swiftcamp.io"
+  },
+
+  {
+    "name": "Tech Transfer Camp",
+    "description": "Search Camp is a community-run single day, single track conference focused on open source search technologies, including Apache Solr and ElasticSearch",
+    "camp": "transfer",
+    "dates": "Wed Jul 13",
+    "icon": "icon-transfer.png",
+    "website": "transfer://swiftcamp.io",
+    "twitter": "https://twitter.com/swiftcamo",
+    "facebook": "",
+    "github": "http://github.com/swiftcamp",
+    "email": "info@swiftcamp.io"
+  },
+
+  {
+    "name": "Web Perf Camp",
+    "description": "",
+    "camp": "webperf",
+    "dates": "",
+    "icon": "icon-webperf.svg",
+    "website": "http://webperf.camp",
+    "twitter": "http://twitter.com/webperfcamp",
+    "facebook": "https://www.facebook.com/Web-Perf-Camp-491647691026324",
+    "github": "http://github.com/webperfcamp",
+    "email": "info@webperf.camp"
+  },
+
+  {
+    "name": "Women in OS",
+    "description": "",
+    "camp": "women",
+    "dates": "Sun Jul 10",
+    "icon": "icon-women.png",
+    "website": "http://webperf.camp",
+    "twitter": "http://twitter.com/webperfcamp",
+    "facebook": "https://www.facebook.com/Web-Perf-Camp-491647691026324",
+    "github": "http://github.com/webperfcamp",
+    "email": "info@webperf.camp"
+  },
+    
+])
--- js/getConferences.js
+++ js/getConferences.js
@@ -9,14 +9,14 @@
 						   '<div class="icon-inner"><a href="' + conferences[i].website + '" target="_blank" class="website"><img src="images/icons/' + conferences[i].icon + '" class="icon"/></a></div>'+
 						 '</div>'+
 						 '<div class="name"><a href="' + conferences[i].website + '" target="_blank" class="website">' + conferences[i].name + '</a></div>'+
-						 '<div class="date">' + conferences[i].dates + '</div>'+
+					 '<div class="date">' + conferences[i].dates + '</div>'+
 						 '<div class="description">' + conferences[i].description + '</div>'+
 						 '<div class="links">'+
 							 '<div class="link website"><a href="' + conferences[i].website + '" target="_blank" class="website"><i class="fa fa-globe"></i></a></div>'+
 							 '<div class="link twitter"><a href="' + conferences[i].twitter + '" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></div>'+    
 							 '<div class="link facebook"><a href="' + conferences[i].facebook + '" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a></div>'+
-//							 '<div class="link"><a href="' + conferences[i].github + '" target="_blank" class="github"><i class="fa fa-github"></i></a></div>'+
-//							 '<div class="link"><a href="mailto:' + conferences[i].email + '" target="_blank" class="email"><i class="fa fa-envelope"></i></a></div>'+
+							 '<div class="link"><a href="' + conferences[i].github + '" target="_blank" class="github"><i class="fa fa-github"></i></a></div>'+
+							 '<div class="link"><a href="mailto:' + conferences[i].email + '" target="_blank" class="email"><i class="fa fa-envelope"></i></a></div>'+
 						 '</div>'+
 					'</div>'+
 				'</div>'+
